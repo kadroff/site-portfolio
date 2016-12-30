62a4c910c418957b7f7e04803deea545fc54429d
Marsel Kadrov <marselkadroff@gmail.com>
2016-12-30 22:42:43 +0500

    test responsible-1.10

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $('#fullpage').fullpage({
-    anchors: ['page1', 'page2'],
+    anchors: ['page1', 'page2','page3'],
     // sectionsColor: ['yellow', 'orange', '#C0C0C0', '#ADD8E6'],
   });
 });
