a32c261e99330ee5071bd0ef8df210106e4a2b82
Marsel Kadrov <marselkadroff@gmail.com>
2017-01-07 22:11:18 +0500

    test responsible-1.21

--- js/app.js
+++ js/app.js
@@ -1,23 +1,5 @@
 $(document).ready(function() {
   $('#fullpage').fullpage({
-    anchors: ['page1', 'page2','page3'],
-    // sectionsColor: ['yellow', 'orange', '#C0C0C0', '#ADD8E6'],
+    anchors: ['header', 'skills','portfolio','contacts'],
   });
 });
-// div.section_2  {
-//   height: 900px;
-// }
-//
-// div.section_2 h1 {
-//  font-size: 70px;
-//  padding-top: 400px;
-// }
-//
-// div.section_2 p {
-//  font-size: 32px;
-// }
-//
-// div.section_2 .skill-image {
-// padding-top: 400px;
-// }
-// }
