44be10a92de04981d70bb706767852df23ab57b0
Marsel Kadrov <marselkadroff@gmail.com>
2017-01-07 22:58:43 +0500

    test responsible-1.22

--- js/app.js
+++ js/app.js
@@ -2,4 +2,11 @@ $(document).ready(function() {
   $('#fullpage').fullpage({
     anchors: ['header', 'skills','portfolio','contacts'],
   });
+
+  $(window).on('load', function () {
+    var $preloader = $('#page-preloader'),
+    $spinner   = $preloader.find('.spinner');
+    $spinner.fadeOut();
+    $preloader.delay(350).fadeOut('slow');
+});
 });
